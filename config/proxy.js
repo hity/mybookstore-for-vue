e7339e6b6b15b1364853a3e10b3d230806d683a1
hity <hity_tt@163.com>
2018-12-25 11:50:58 +0800

    Update proxy.js

--- config/proxy.js
+++ config/proxy.js
@@ -6,11 +6,7 @@ module.exports = {
         '/api/book': RAP_SERVICE + '170',
         '/api/tag': RAP_SERVICE + '170',
         '/api/pos': RAP_SERVICE + '170',
-        '/api/category': RAP_SERVICE + '170',
-        '/common/provinces': 'https://m-testper.aiyoumi.com:10454',
-        '/common/citys': 'https://m-testper.aiyoumi.com:10454',
-        '/common/districts': 'https://m-testper.aiyoumi.com:10454',
-        '/common/streets': 'https://m-testper.aiyoumi.com:10454'
+        '/api/category': RAP_SERVICE + '170'
     },
     // 本地json数据接口, 如果需要根据url的参数区分不同的接口数据，那么url中带？，并且指定具体的接口
     proxyLocal: {
